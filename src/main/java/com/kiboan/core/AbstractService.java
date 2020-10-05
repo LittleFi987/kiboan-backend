35a706229266bcf2c8639f512b0ee0ba7f68e01d
chenhao.ye <yechenhao1996@gmail.com>
2020-10-05 21:03:06 +0800

    refine code

--- src/main/java/com/kiboan/core/AbstractService.java
+++ src/main/java/com/kiboan/core/AbstractService.java
@@ -1,4 +1,4 @@
-package com.company.project.core;
+package com.kiboan.core;
 
 
 import org.apache.ibatis.exceptions.TooManyResultsException;
--- src/main/java/com/kiboan/core/Result.java
+++ src/main/java/com/kiboan/core/Result.java
@@ -1,4 +1,4 @@
-package com.company.project.core;
+package com.kiboan.core;
 
 import com.alibaba.fastjson.JSON;
 
